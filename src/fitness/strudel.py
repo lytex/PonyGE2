39ba2bd6241b8683b71bdb3b0af55fabc2557f53
lytex <julianlpc15@gmail.com>
2024-03-09 17:08:27 +0100

    working v2

--- src/fitness/strudel.py
+++ src/fitness/strudel.py
@@ -1,4 +1,5 @@
 from fitness.base_ff_classes.base_ff import base_ff
+import subprocess
 
 
 class strudel(base_ff):
@@ -12,4 +13,16 @@ class strudel(base_ff):
         super().__init__()
 
     def evaluate(self, ind, **kwargs):
+        js_code = """ import { evaluate_mini } from "./evaluate.mjs";""" \
+        f"""let mini_code = "{ind.phenotype}";
+        console.log(evaluate_mini(mini_code, 4).join("\\n")); """
+        with open("../../scratchpad.mjs", "w") as f:
+            f.write(js_code)
+        result = subprocess.check_output("node ../../scratchpad.mjs", shell=True, stderr=subprocess.DEVNULL)
+        midi = [int(x) for x in result.decode('utf-8').replace('🌀 @strudel/core loaded 🌀\n', '').split('\n') if x]
+        print(midi)
+
         return ind.nodes
+
+
+
